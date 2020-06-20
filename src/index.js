90216fa2a507fd6a3f925bf75553ee98c07a6cdf
Aidan King <aidan.king151@gmail.com>
2020-06-20 16:42:15 +0100

    use reducer

--- src/index.js
+++ src/index.js
@@ -1,16 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import CharacterList from './CharacterList';
-
-import dummyData from './dummy-data';
-
 import './styles.scss';
+import endpoint from './endpoint';
+
+const LOADING = 'LOADING';
+const RESPONSE_COMPLETE = 'RESPONSE_COMPLETE';
+const ERROR = 'ERROR';
+
+const initialState = {
+  result: null,
+  loading: true,
+  error: null,
+};
+
+const fetchReducer = (state, action) => {
+  if (action.type === LOADING) {
+    return {
+      result: null,
+      loading: true,
+      error: null,
+    };
+  }
+
+  if (action.type === RESPONSE_COMPLETE) {
+    return {
+      result: action.payload.response,
+      loading: false,
+      error: null,
+    };
+  }
+
+  if (action.type === ERROR) {
+    return {
+      result: null,
+      loading: false,
+      error: action.payload.error,
+    };
+  }
+  return state;
+};
+
+const useFetch = (url) => {
+  const [state, dispatch] = useReducer(fetchReducer, initialState);
+
+  useEffect(() => {
+    dispatch({
+      type: LOADING,
+    });
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => {
+        dispatch({
+          type: RESPONSE_COMPLETE,
+          payload: { response },
+        });
+      })
+      .catch((error) => {
+        dispatch({
+          type: ERROR,
+          payload: { error },
+        });
+      });
+  }, []);
+
+  return [state.result, state.loading, state.error];
+};
 
 const Application = () => {
-  const [characters, setCharacters] = useState(dummyData);
+  const [response, loading, error] = useFetch(endpoint + '/characters');
+
+  const characters = (response && response.characters) || [];
 
   return (
     <div className="Application">
@@ -19,7 +83,12 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
-          <CharacterList characters={characters} />
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <CharacterList characters={characters} />
+          )}
+          {error && <p className="error">{error.message}</p>}
         </section>
       </main>
     </div>
