7d8f6dbb24fa5588a6e95568c4bf4485a1c4da48
Aidan King <aidan.king151@gmail.com>
2020-06-21 17:11:44 +0100

    thunk hook

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect, useCallback } from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import CharacterList from './CharacterList';
+import isFunction from 'lodash/isFunction';
 
 import endpoint from './endpoint';
 
@@ -37,15 +38,51 @@ const reducer = (state, action) => {
   return state;
 };
 
+const fetchCharacters = (dispatch) => {
+  dispatch({ type: 'LOADING' });
+  fetch(endpoint + '/characters')
+    .then((response) => response.json())
+    .then((response) =>
+      dispatch({
+        type: 'RESPONSE_COMPLETE',
+        payload: { characters: response.characters },
+      }),
+    )
+    .catch((error) => dispatch({ type: 'ERROR', payload: { error } }));
+};
+
 const initialState = {
   error: null,
   loading: false,
   characters: [],
 };
 
-const Application = () => {
+const useThunkReducer = (reducer, initialState) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const thunkDispatch = useCallback(
+    (action) => {
+      console.log(action);
+
+      if (isFunction(action)) {
+        action(dispatch);
+      } else {
+        dispatch(action);
+      }
+    },
+    [dispatch],
+  );
+
+  return [state, thunkDispatch];
+};
+
+const Application = () => {
+  const [state, dispatch] = useThunkReducer(reducer, initialState);
   const { characters } = state;
+
+  useEffect(() => {
+    dispatch((dispatch) => {});
+  }, []);
 
   return (
     <div className="Application">
@@ -54,7 +91,9 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
-          <button onClick={() => {}}>Fetch Characters</button>
+          <button onClick={() => dispatch(fetchCharacters)}>
+            Fetch Characters
+          </button>
           <CharacterList characters={characters} />
         </section>
       </main>
